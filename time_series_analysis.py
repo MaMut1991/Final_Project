9d9cac6e6231cd64c5ad009e9dce87ce22ae61b5
Mario <doctorbeat01@gmail.com>
2024-09-18 19:32:56 +0200

    Farbpalette in Zeitreihenanalyse hinzugefügt

--- time_series_analysis.py
+++ time_series_analysis.py
@@ -15,15 +15,24 @@ def get_time_series():
     sales4 = 0
     sales6 = 0
 
+    # Größe Schriftzüge Achsen, Titel
     fontsize_title = 20
     fontsize_axes =15
+
+    #Farbpaletten
+    color_palette_1 = ['#763DFF']    # 1 Farbe für Diagramm
+    color_palette_2 = ['#763DFF', '#FF3D65']    # 2 Farben für Diagramm
+    color_palette_3 = ['#763DFF', '#FF3D65', '#C6FF3D']    # 3 Farben für Diagramm
+    color_palette_4 = ['#763DFF', '#FF3D65', '#C6FF3D', '#3DFFD7']    # 4 Farben für Diagramm
+    color_palette_5 = []    # 5 Farben für Diagramm
+    color_palette_6 = []    # 6 Farben für Diagramm
 
     # Daten beschaffen aus preprocessing.py
     merge_train, merge_test = data_preprocessing()
 
     # 2. EDA - Zeitabhängige Features plotten
     fig, ax = plt.subplots(figsize=(15, 8))
-    merge_train[['Date', 'Temperature', 'Fuel_Price', 'CPI', 'Unemployment']].plot(x='Date', subplots=True, ax=ax, title = 'Zeitabhägige Features')
+    merge_train[['Date', 'Temperature', 'Fuel_Price', 'CPI', 'Unemployment']].plot(x='Date', subplots=True, ax=ax, color=color_palette_4, title = 'Zeitabhägige Features')
     ax.set_title('Zeitabhägige Features', fontsize=fontsize_title)
     st.pyplot(fig)
 
